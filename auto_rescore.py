7fd76d35280e538bc3d365d0a27bc85d9f92f2f7
kalabharath <kalabharath@gmail.com>
2015-05-18 15:14:23 +1000

    using ts4 broker instead of ts3 broker

--- auto_rescore.py
+++ auto_rescore.py
@@ -2,7 +2,7 @@ import sys, os
 protein=(os.popen("sed -n '1p' config.txt").read()).rstrip()
 iter=(os.popen("sed -n '3p' config.txt").read()).rstrip()
 EXECUTABLE="/short/xc4/kbp502/gps4rosetta/Rosetta/main/source/bin/score.linuxgccrelease"
-BROKER_FLAG="-broker::setup ../setup/broker-ts3.txt -run:protocol broker -overwrite"
+BROKER_FLAG="-broker::setup ../setup/broker-ts34.txt -run:protocol broker -overwrite"
 DATABASE="-database /short/xc4/kbp502/gps4rosetta/Rosetta/main/database"
 FULL="-in:file:silent ./"+protein+".silent_file"
 WEIGHTS="-score:weights pcsweight.patch"
