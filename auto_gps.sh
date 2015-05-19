4082f75ac0b654e79c434f384d4f3d976936a328
kalabharath <kalabharath@gmail.com>
2015-05-19 10:44:25 +1000

    largest number of cpus & jobs (1008 for 2012 structures)

--- auto_gps.sh
+++ auto_gps.sh
@@ -1,8 +1,9 @@
+#!/usr/bin/env bash
 #PBS -P xc4
 #PBS -q normal
-#PBS -l walltime=5:00:00
+#PBS -l walltime=10:00:00
 #PBS -l mem=2000GB
-#PBS -l ncpus=1024
+#PBS -l ncpus=1008
 #PBS -l wd
 
 module load openmpi
@@ -12,7 +13,7 @@ database="-database /short/xc4/kbp502/gps4rosetta/Rosetta/main/database"
 protein=$(sed -n '1p' config.txt)
 arg1="-abinitio::increase_cycles 5"
 arg2="-abinitio::rg_reweight 0.5 -abinitio::rsd_wt_helix 0.5 -abinitio::rsd_wt_loop 0.5 -abinitio::use_filters false"
-arg3="-nstruct 2046"
+arg3="-nstruct 2012"
 arg4="-mute all"
 arg5="-frag9 ./frag9_${protein}_r0.tab"
 arg6="-frag3 ./frag3_${protein}_r0.tab"
@@ -26,7 +27,7 @@ patch5="-abinitio::stage4_patch ${protein}_r0.wts"
 
 patch_flag="$patch1 $patch2 $patch3 $patch4 $patch5"
 broker_flag="-broker::setup ../setup/broker-ts4.txt -run:protocol broker -overwrite"
-mpirun="mpirun -np 1024"
+mpirun="mpirun -np 1008"
 run="$mpirun $Executable $database $arg1 $arg2 $arg3 $arg4 $arg5 $arg6 $arg7 $arg8 $arg9 $arg10 $patch_flag $broker_flag $exclude"
 echo $run
 $run
--- auto_iter.sh
+++ auto_iter.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #PBS -P xc4
 #PBS -q normal
-#PBS -l walltime=5:00:00
+#PBS -l walltime=10:00:00
 #PBS -l mem=2000GB
-#PBS -l ncpus=1024
+#PBS -l ncpus=1008
 #PBS -l wd
 
 module load openmpi
@@ -14,7 +14,7 @@ protein=$(sed -n '1p' config.txt)
 iter=$(sed -n '3p' config.txt)
 arg1="-abinitio::increase_cycles 4.0"
 arg2="-abinitio::rg_reweight 0.5 -abinitio::rsd_wt_helix 0.5 -abinitio::rsd_wt_loop 0.5 -abinitio::use_filters false"
-arg3="-nstruct 2044"
+arg3="-nstruct 2012"
 arg4="-mute all"
 arg5="-frag9 ./frag9_${protein}_r${iter}.tab"
 arg6="-frag3 ./frag3_${protein}_r${iter}.tab"
@@ -30,7 +30,7 @@ patch5="-abinitio::stage4_patch ${protein}_r${iter}.wts"
 patch_flag="$patch1 $patch2 $patch3 $patch4 $patch5"
 broker_flag="-broker::setup ../setup/broker-ts4.txt -run:protocol broker -overwrite"
 
-mpirun="mpirun -np 1024"
+mpirun="mpirun -np 1008"
 run="$mpirun $Executable $database $arg1 $arg2 $arg3 $arg4 $arg5 $arg6 $arg7 $arg8 $arg9 $arg10 $patch_flag $broker_flag"
 echo $run
 $run
